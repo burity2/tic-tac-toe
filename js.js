85961fc8b87e06d16a503c7a237aff11c21376e6
burity2 <arthur.rosa66@gmail.com>
2025-11-10 17:46:16 -0300

    game logic finished, game reset to be added

--- js.js
+++ js.js
@@ -1,8 +1,9 @@
 function gameBoard(){
+  // creates the board
   const rows = 3;
   const columns = 3;
   const board = [];
-
+  // for loop for 2d arrays
   for(i = 0; i < rows; i++){
     board[i] = [];
       for(j = 0; j < columns; j++){
@@ -10,6 +11,7 @@ function gameBoard(){
       };
   };
 
+  //marks the board and return marked board
   const addMark = function(mark, row, column){
     if (board[row][column] != 0) {
     } else {
@@ -18,8 +20,10 @@ function gameBoard(){
     };
   };
 
+  //used to get the board on new instances
   const getBoard = () => board;
 
+  //prints the board
   const printBoard = () => {
     let printedBoard = gameBoard().getBoard();
     console.log(printedBoard);
@@ -28,7 +32,9 @@ function gameBoard(){
   return { getBoard, addMark, printBoard };
 };
 
+
 function createPlayers(player1 = "Player One", player2 = "Player Two"){
+  //creates two autonamed players
   return [
     { name: player1, mark: "X" }, 
     { name: player2, mark: "O" }
@@ -37,33 +43,70 @@ function createPlayers(player1 = "Player One", player2 = "Player Two"){
 
 function GameControl(){
 
+//create the players and calls the new gameboard instance
 const playerArr = createPlayers();
 const board = gameBoard();
 
+//switch between players
 let activePlayer = playerArr[0];
-
-const newRound = function(){
-console.log(`It's ${activePlayer.name}'s turn`);
-}
-
 
 const playerSwitcher = function() {
   activePlayer = activePlayer === playerArr[0] ? playerArr[1] : playerArr[0];
 };
 
+//call for the player of current round
+const newRound = function(){
+console.log(`It's ${activePlayer.name}'s turn`);
+}
+
+//used to mark the board
 const playRound = function(row, column) {
 const marked = board.addMark(activePlayer.mark, row, column)
 
+//checks for vitories
 const victoryChecker = function(){
   const treeXInARow = marked.join(" ").includes("X,X,X");
   const treeOInARow = marked.join(" ").includes("O,O,O");
+  const tie = marked.join("").includes(0)
 
+  let gameFinisher = () => {
+    playerSwitcher();
+    console.log(`Game over, ${activePlayer.name} wins!`);
+  }
+
+  let gameTie = () => {
+    console.log(`Game over, it's a tie!`)
+  }
+
+  //checks columns
+  for (i = 0; i < 3; i++){
+    marked[0][i] != 0 &&
+    marked[0][i] == marked[1][i] &&
+    marked[1][i] == marked[2][i] ?
+    gameFinisher() : false
+  };
+//checks rows
   if (treeOInARow || treeXInARow) {
-    console.log("Eureka!")
-  } else if ((marked[0][0] == marked[1][0] == marked[2][0]) || (marked[0][1] == marked[1][1] == marked[2][1]) || (marked[0][2] == marked[1][2] == marked[2][2]) && marked != 0) {
-    console.log("dumb eureka...")
-  } else {
-
+    gameFinisher();
+// checks diagonal 1
+  } else if ( marked[0][0] != 0 &&
+    marked[0][0] == marked[1][1] &&
+    marked[1][1] == marked[2][2]){
+    gameFinisher();
+// checks diagonal 2
+  } else if ( marked[0][2] != 0 &&
+    marked[0][2] == marked[1][1] &&
+    marked[1][1] == marked[2][0]) {
+    gameFinisher();
+  } else if(marked[0][0] != 0) {
+    for (i = 0; i < 3; i++){
+    marked[0][i] != 0 &&
+    marked[0][i] == marked[1][i] &&
+    marked[1][i] == marked[2][i] ?
+    gameFinisher() : false
+  };
+  } else if (tie == false){
+    gameTie();
   };
   
 };
@@ -83,97 +126,3 @@ return { playRound }
 };
 
 let test = GameControl();
-
-/* 
-//THE REAL DEAL WORK-O-MOJO
-function gameBoard() {
-    const rows = 3;
-    const columns = 3;
-    const board = [];
-
-    for (let i = 0; i < rows; i++) {
-    board[i] = [];
-    for (let j = 0; j < columns; j++) {
-      board[i].push(cell());
-        }
-    };
-
-    const getBoard = () => board;
-
-const addMark = (row, column, player) => {
-    if (board[row][column].getValue() === 0) { 
-        board[row][column].marker(player);
-        return true; 
-    }
-    return false;
-};
-
-    const printBoard = () => {
-    const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
-    console.log(boardWithCellValues);
-  };
-
-  return { getBoard, addMark, printBoard }
-};
-
-function cell() {
-  let value = 0;
-
-  const marker = (player) => {
-    value = player;
-  };
-
-  const getValue = () => value;
-
-  return { marker, getValue };
-};
-
-function GameController(
-  playerOneName = "Player One",
-  playerTwoName = "Player Two"
-) {
-  const board = gameBoard();
-
-  const players = [
-    {
-      name: playerOneName,
-      mark: "X"
-    },
-    {
-      name: playerTwoName,
-      mark: "O"
-    }
-  ];
-
-  let activePlayer = players[0];
-
-  const switchPlayerTurn = () => {
-    activePlayer = activePlayer === players[0] ? players[1] : players[0];
-  };
-  const getActivePlayer = () => activePlayer;
-
-  const printNewRound = () => {
-    board.printBoard();
-    console.log(`${getActivePlayer().name}'s turn.`);
-  };
-
-const playRound = (row, column) => {
-    const marked = board.addMark(row, column, getActivePlayer().mark);
-    
-    if (marked) {
-        switchPlayerTurn();
-        printNewRound();
-    } else {
-    }
-};
-
-  printNewRound();
-
-  return {
-    playRound,
-    getActivePlayer
-  };
-}
-
-const game = GameController();
- */
