e6ad820d3bae8db77cb004f1ecb16347566623d4
burity2 <arthur.rosa66@gmail.com>
2025-11-18 13:55:44 -0300

    almost finished, player's names to fix

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # tic-tac-toe
+11.17 - Had trouble with the "resetGame" function. Now the first player always changes, therefore "X" doesn't start always.
--- js.js
+++ js.js
@@ -34,6 +34,7 @@ function gameBoard(){
 
 
 function createPlayers(player1 = "Player One", player2 = "Player Two"){
+
   return [
     { name: player1, mark: "X" }, 
     { name: player2, mark: "O" }
@@ -55,7 +56,9 @@ const playerSwitcher = function() {
 
 const resetGame = function(){
   board = gameBoard();
-  activePlayer = playerArr[0]
+  document.getElementById("game-area").style.pointerEvents = "auto";
+  // activePlayer = playerArr[0]
+  playerSwitcher();
   turnDeclaration();
 }
 
@@ -77,19 +80,29 @@ const victoryChecker = function(){
 
   let gameFinisher = () => {
     playerSwitcher();
-    const replay = confirm(`${activePlayer.name} wins! Play again?`);
-    if (replay) {
-      resetGame();
-      return;
+
+    setTimeout(() => {
+      let replay = confirm(`${activePlayer.name} wins! Play again?`, 1000);
+    if (replay == true) {
+          if (activePlayer == playerArr[0]){
+            document.getElementById("player-one-score").innerHTML++
+          } else {
+            document.getElementById("player-two-score").innerHTML++
+          }
+          resetGame();
+          let btns = document.getElementsByClassName("game-btn");
+          for (let i = 0; i < btns.length; i++){
+            btns[i].innerHTML = "&nbsp"
+          };
+    } else {
+      let gamebtns = document.getElementById("game-area");
+      gamebtns.style.pointerEvents = "none";
     }
+  }, 0)
   };
 
   let gameTie = () => {
-    const replay = confirm(`Game over, it's a tie! Play again?`);
-    if (replay) {
-      resetGame();
-      return;
-    }
+    console.log(`It's a tie!`);
   };
 
 //checks columns
@@ -107,12 +120,12 @@ let col = false;
 //checks rows
   if (treeOInARow || treeXInARow) {
     gameFinisher();
-// checks diagonal 1
+// checks 1st diagonal 
   } else if ( marked[0][0] != 0 &&
     marked[0][0] == marked[1][1] &&
     marked[1][1] == marked[2][2]){
     gameFinisher();
-// checks diagonal 2
+// checks 2nd diagonal
   } else if ( marked[0][2] != 0 &&
     marked[0][2] == marked[1][1] &&
     marked[1][1] == marked[2][0]) {
@@ -138,10 +151,124 @@ if (marked){
 
 turnDeclaration();
 
-return { playRound }
+return { playRound, resetGame }
+};
+
+function gameInterface(){
+
+  const playerArr = createPlayers()
+  let activePlayer = playerArr[0];
+
+  let playerOneBox = document.getElementById("player-one-name");
+  let playerTwoBox = document.getElementById("player-two-name");
+
+  playerOneBox.style.borderBottomColor = "red";
+  playerOneBox.style.borderBottomStyle = "solid";
+
+  const playerSwitcher = () => {
+    activePlayer = activePlayer === playerArr[0] ? playerArr[1] : playerArr[0];
+
+      if (activePlayer == playerArr[0]) {
+      playerOneBox.style.borderBottomColor = "red";
+      playerOneBox.style.borderBottomStyle = "solid";
+      playerTwoBox.style.border = "none";
+    } else {
+      playerTwoBox.style.borderBottomColor = "red";
+      playerTwoBox.style.borderBottomStyle = "solid";
+      playerOneBox.style.border = "none";
+    }
+  };
+  
+  let newGame = () => {
+    window.location.reload();
+  }
+  document.getElementById("new-game-btn").addEventListener("click", newGame);
+
+  document.getElementById("container").addEventListener("click", (e) => {
+    let target = e.target;
+
+    let btn = () => {
+      target.innerHTML = `${activePlayer.mark}`
+      playerSwitcher();
+    }
+
+    if (target.innerHTML == "X" || target.innerHTML == "O") {    
+    } else {
+        switch(target.id) {
+
+        case "00": 
+        btn();
+        playGame.playRound(0,0)
+          break;
+        
+        case "01": 
+        btn();
+        playGame.playRound(0,1)
+          break;
+        
+        case "02": 
+        btn();
+        playGame.playRound(0,2)
+          break;
+        
+        case "10": 
+        btn();
+        playGame.playRound(1,0)
+          break;
+        
+        case "11": 
+        btn();
+        playGame.playRound(1,1)
+          break;
+        
+        case "12": 
+        btn();
+        playGame.playRound(1,2)
+          break;
+        
+        case "20": 
+        btn();
+        playGame.playRound(2,0)
+          break;
+        
+        case "21": 
+        btn();
+        playGame.playRound(2,1)
+          break;
+        
+        case "22": 
+        btn();
+        playGame.playRound(2,2)
+          break;
+
+        // case "reset-btn": playGame.resetGame();
+        //   playerSwitcher();
+        //   let btns = document.getElementsByClassName("game-btn");
+        //   for (let i = 0; i < btns.length; i++){
+        //     btns[i].innerHTML = "&nbsp"
+        //   };
+        //   break
+
+        case "player-one-name":
+          let playerOneName = prompt("Player 1 name:", "player 1");
+          if (playerOneName != null) {
+            target.innerHTML = playerOneName;
+          };
+          break;
+
+        case "player-two-name":
+          let playerTwoName = prompt("Player 2 name:", "player 2");
+          if (playerTwoName != null) {
+            target.innerHTML = playerTwoName;
+          };
+          break;
+      };
+    };
+});
 };
 
 (function () {
+gameInterface();
 playGame = gameControl();
 return playGame
 })();
